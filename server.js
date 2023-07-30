aca199c8c5baee418375b451c1b015c380db93c5
Shoumikh <ssoumikh@gmail.com>
2023-07-30 12:44:14 +0600

    ADD:: log and error tracker

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/ErrorHandler");
 const PORT = process.env.PORT || 3500;
+
+app.use(logger);
 
 app.use("/", express.static(path.join(__dirname, "/public")));
 
@@ -17,6 +21,8 @@ app.all("*", (req, res) => {
     res.type("txt").send("404 Not Found");
   }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);

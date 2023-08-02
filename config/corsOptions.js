3c92b991e627482dafe2cf05df995933ca0d7a39
Shoumikh <ssoumikh@gmail.com>
2023-08-02 13:45:43 +0600

    ADD :: DB connection and users crud

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,11 +1,28 @@
 const allowedOrigins = require("./allowedOrigins");
 
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     // allow requests with no origin
+//     // (like mobile apps or curl requests)
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       const msg =
+//         "The CORS policy for this site does not " +
+//         "allow access from the specified Origin.";
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   },
+// };
+
 const corsOptions = {
   origin: (origin, callback) => {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
-      callback(new Error("Not allowed by CORS"));
+      callback(null, true);
+
+      // callback(new Error("Not allowed by CORS"));
     }
   },
   credentails: true,
